47107f1fd5bdfa488505e2ba172c6ecf3c252295
unknown <jambukiyavishal547@gmail.com>
2023-08-22 12:31:26 +0530

    eccomerce

---
+++ ecommerce/views.py
@@ -0,0 +1,445 @@
+from django.shortcuts import render,redirect
+from .models import User,Product,Wishlist,Cart
+import requests
+import random
+import stripe
+from django.conf import settings
+from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+	
+# Create your views here.
+	
+stripe.api_key = settings.STRIPE_PRIVATE_KEY
+YOUR_DOMAIN = 'http://localhost:8000'
+
+def validate_signup(request):
+	email=request.GET.get('email')
+	data={
+	'is_taken':User.objects.filter(email__iexact=email).exists()
+	}
+	return JsonResponse(data)
+
+def validate_old_password(request):
+	oldpassword=request.GET.get('oldpassword')
+	user=User.objects.get(email=request.session['email'])
+	flag=False
+	if user.password==oldpassword:
+		flag=True
+	data={
+	'is_taken':flag
+	}
+	return JsonResponse(data)
+
+def validate_cnew_password(request):
+	cnewpassword=request.GET.get('cnewpassword')
+	newpassword=request.GET.get('newpassword')
+	user=User.objects.get(email=request.session['email'])
+	flags=False
+	if newpassword==cnewpassword:
+		flag=True
+	data={
+	'is_taken':User.objects.filter(password=cnewpassword).exists()
+	}
+	return JsonResponse(data)
+
+def validate_product_name(request):
+	product_name=request.GET.get('products_name')
+	seller=User.objects.get(email=request.session['email'])
+	data={
+	'is_taken':Product.objects.filter(product_name=product_name,seller=seller).exists()
+	}
+	return JsonResponse(data) 
+
+@csrf_exempt
+def create_checkout_session(request):
+	amount = int(json.load(request)['post_data'])
+	final_amount=amount*100
+	
+	session = stripe.checkout.Session.create(
+		payment_method_types=['card'],
+		line_items=[{
+			'price_data': {
+				'currency': 'inr',
+				'product_data': {
+					'name': 'Checkout Session Data',
+					},
+				'unit_amount': final_amount,
+				},
+			'quantity': 1,
+			}],
+		mode='payment',
+		success_url=YOUR_DOMAIN + '/success.html',
+		cancel_url=YOUR_DOMAIN + '/cancel.html',)
+	return JsonResponse({'id': session.id})
+
+def success(request):
+	user=User.objects.get(email=request.session['email'])
+	carts=Cart.objects.filter(user=user,payment_status=False)
+	for i in carts:
+		i.payment_status=True
+		i.save()
+		
+	carts=Cart.objects.filter(user=user,payment_status=False)
+	request.session['cart_count']=len(carts)
+	return render(request,'success.html')
+
+def cancel(request):
+	return render(request,'cancel.html')
+
+
+def index(request):
+	try:
+		products=Product.objects.all()
+		user=User.objects.get(email=request.session['email'])
+		if user.usertype=="buyer":
+			return render(request,'index.html',{'products':products})
+		else:
+			return render(request,'seller-index.html',{'products':products})
+	except:
+		return render(request,'index.html',{'products':products})
+		
+
+def seller_index(request):
+	return render(request,'seller-index.html')
+
+def checkout(request):
+	return render(request,'checkout.html')
+
+
+def singup(request):
+	if request.method=='POST':
+		try:
+			User.objects.get(email=request.POST['email'])
+			msg="Email Alredy Exist"
+			return render(request,'singup.html',{'msg':msg})
+		except:
+			if request.POST['password']==request.POST['cpassword']:
+				if request.POST['usertype']=="buyer":
+					admin_access=True
+				else:
+					admin_access=False
+				User.objects.create(
+					fname=request.POST['fname'],
+					lname=request.POST['lname'],
+					email=request.POST['email'],
+					mobile=request.POST['mobile'],
+					address=request.POST['address'],
+					password=request.POST['password'],
+					profile_pic=request.FILES['profile_pic'],
+					usertype=request.POST['usertype'],
+					admin_access=admin_access
+				)
+				msg="User Sing Up Successfully"
+				return render(request,'singin.html',{'msg':msg})
+			else:
+				msg="Password Not Match"
+				return render(request,'singup.html',{'msg':msg})
+	else:
+		return render(request,'singup.html')
+
+def singin(request):
+	if request.method=="POST":
+		try:
+			user=User.objects.get(email=request.POST['email'])
+			if user.password==request.POST['password']:
+				if user.usertype=="buyer":
+					request.session['email']=user.email
+					request.session['fname']=user.fname
+					request.session['profile_pic']=user.profile_pic.url
+					request.session['new-password']=user.new-password
+					wishlists=Wishlist.objects.filter(user=user)
+					request.session['wishlist_count']=len(wishlists)
+					carts=Cart.objects.filter(user=user, payment_status=False)
+					request.session['cart_count']=len(carts)
+					return redirect('index')
+				else:
+					if user.admin_access==True:
+						request.session['email']=user.email
+						request.session['fname']=user.fname
+						request.session['profile_pic']=user.profile_pic.url
+						return render(request,'seller-index.html')
+					else:
+						msg="Your admin access in still not approved, please contect admin"
+						return render(request,'singin.html',{'msg':msg})
+			else:
+				msg="Incorrect Password"
+				return render(request,'singin.html',{'msg':msg})
+		except:
+			msg="Email Not Registered"
+			return render(request,'singin.html',{'msg':msg})
+	else:
+		return render(request,'singin.html')
+
+def singout(request):
+	try:
+		del request.session['email']
+		del request.session['fname']
+		return render(request,'singin.html')
+	except:
+		return render(request,'singin.html')
+
+def change_password(request):
+	user=User.objects.get(email=request.session['email'])
+	if request.method=="POST":
+		if user.password==request.POST['old-password']:
+			if request.POST['new-password']==request.POST['c-password']:
+				user.password=request.POST['new-password']
+				user.save()
+				return redirect('singout')
+			else:
+				msg="Password Not Match"
+				if user.usertype=="buyer":
+					return render (request,'change-password.html',{'msg':msg})
+				else:
+					return render (request,'seller-change-password.html',{'msg':msg})
+		else:
+			msg="Old Password Not Match"
+			if user.usertype=="buyer":
+					return render (request,'change-password.html',{'msg':msg})
+			else:
+				return render (request,'seller-change-password.html',{'msg':msg})
+	else:
+		if user.usertype=="buyer":
+				return render (request,'change-password.html')
+		else:
+				return render (request,'seller-change-password.html')
+
+def seller_change_password(request):
+	return render(request,'seller_change_password.html')
+
+def forgot_password(request):
+	if request.method=="POST":
+		mobile=request.POST['mobile']
+		try:
+			user=User.objects.get(mobile=mobile)
+			otp=random.randint(1000,9999)
+			url = "https://www.fast2sms.com/dev/bulkV2"
+			querystring = {"authorization":"mIk9Ya7tBzZTeAU5C3VGyLwJuRH8ogc2xrqnsKMdPSib6W0jNlQCyZJVL4px2luo8hKgraUk9b3GWejM","variables_values":str(otp),"route":"otp","numbers":str(mobile)}
+			headers = {'cache-control': "no-cache"}
+			response = requests.request("GET", url, headers=headers, params=querystring)
+			print(response.text)
+			return render(request,'otp.html',{'otp':otp,'mobile':mobile})
+
+		except:
+			msg="Mobile Not Registered"
+			return render(request,'forgot-password.html',{'msg':msg})
+	else:
+		return render(request,'forgot-password.html')
+
+def verify_otp(request):
+	otp=request.POST['otp']
+	uotp=request.POST['uotp']
+	mobile=request.POST['mobile']
+
+	if otp==uotp:
+		return render(request,'new-password.html',{'mobile':mobile})
+	else:
+		msg="Incorrect otp"
+		return render(request,'otp.html',{'otp':otp,'mobile':mobile,'msg':msg})
+
+def new_password(request):
+	mobile=request.POST['mobile']
+	np=request.POST['new-password']
+	cnp=request.POST['c-password']
+
+	if np==cnp:
+		user=User.objects.get(mobile=mobile)
+		user.password=np
+		user.save()
+		msg="Password Updated successfully"
+		return render(request,'singin.html',{'msg':msg})
+	else:
+		msg="Password Not Match"
+		return render(request,'new-password.html',{'mobile':mobile,'msg':msg})
+
+def product(request):
+		return render(request,'product.html')
+
+def store(request):
+		return render(request,'store.html')
+
+def profile(request):
+	user=User.objects.get(email=request.session['email'])
+	if request.method=="POST":
+		user.fname=request.POST['fname']
+		user.lname=request.POST['lname']
+		user.email=request.POST['email']
+		user.mobile=request.POST['mobile']
+		user.address=request.POST['address']
+		try:
+			user.profile_pic=request.FILES['profile_pic']
+		except:
+			pass
+		user.save()
+		request.session['profile_pic']=user.profile_pic.url
+		msg="profile Updated successfully"
+		if user.usertype=="buyer":
+			return render(request,'profile.html',{'user':user,'msg':msg})
+		else:
+			return render(request,'seller-profile.html',{'user':user,'msg':msg})
+	else:
+		if user.usertype=="buyer":
+			return render(request,'profile.html',{'user':user})
+		else:
+			return render(request,'seller-profile.html',{'user':user})
+
+
+def seller_add_product(request):
+	seller=User.objects.get(email=request.session['email'])
+	if request.method=="POST":
+		Product.objects.create(
+				seller=seller,
+				product_category=request.POST['product_category'],
+				product_name=request.POST['product_name'],
+				product_price=request.POST['product_price'],
+				product_desc=request.POST['product_desc'],
+				product_image=request.FILES['product_image'],
+			)
+		msg="Product Added Successfully"
+		return render(request,'seller-add-product.html',{'msg':msg})
+	else:
+		return render(request,'seller-add-product.html')
+
+def seller_view_product(request):
+	seller=User.objects.get(email=request.session['email'])
+	products=Product.objects.filter(seller=seller)
+	return render(request,'seller-view-product.html',{'products':products})
+
+def seller_product_details(request,pk):
+	product=Product.objects.get(pk=pk)	
+	return render(request,'seller-product-details.html',{'product':product})
+
+def seller_edit_product(request,pk):
+	product=Product.objects.get(pk=pk)
+	if request.method=="POST":
+		product.product_category=request.POST['product_category']
+		product.product_name=request.POST['product_name']
+		product.product_price=request.POST['product_price']
+		product.product_desc=request.POST['product_desc']
+		try:
+			product.product_image=request.FILES['product_image']
+		except:
+			pass
+		product.save()
+		msg="Product Updated successfully"
+		return render(request,'seller-edit-product.html',{'product':product,'msg':msg})
+	else:
+		return render(request,'seller-edit-product.html',{'product':product})
+
+def seller_delete_product(request,pk):
+	product=Product.objects.get(pk=pk)
+	product.delete()
+	return redirect('seller-view-product')
+
+
+def seller_view_laptop(request):
+	seller=User.objects.get(email=request.session['email'])
+	products=Product.objects.filter(seller=seller,product_category="Laptop")
+	return render(request,'seller-view-product.html',{'products':products})
+
+
+def seller_view_accessories(request):
+	seller=User.objects.get(email=request.session['email'])
+	products=Product.objects.filter(seller=seller,product_category="Accessories")
+	return render(request,'seller-view-product.html',{'products':products})
+
+
+def seller_view_camera(request):
+	seller=User.objects.get(email=request.session['email'])
+	products=Product.objects.filter(seller=seller,product_category="Camera")
+	return render(request,'seller-view-product.html',{'products':products})
+
+def product_details(request,pk):
+	wishlist_flag=False
+	cart_flag=False
+	product=Product.objects.get(pk=pk)
+	user=User.objects.get(email=request.session['email'])
+	try:
+		Wishlist.objects.get(user=user,product=product)
+		wishlist_flag=True
+	except:
+		pass
+	try:
+		Cart.objects.get(user=user,product=product,payment_status=False)
+		cart_flag=True
+	except:
+		pass
+	return render(request,'product-details.html',{'product':product,'wishlist_flag':wishlist_flag,'cart_flag':cart_flag})
+
+
+def add_to_wishlist(request,pk):
+	product=Product.objects.get(pk=pk)
+	user=User.objects.get(email=request.session['email'])
+	Wishlist.objects.create(
+			user=user,
+			product=product
+		)
+	return redirect('wishlist')
+
+def wishlist(request):
+	user=User.objects.get(email=request.session['email'])
+	wishlists=Wishlist.objects.filter(user=user)
+	request.session['wishlist_count']=len(wishlists)
+	return render(request,'wishlist.html',{'wishlists':wishlists})
+
+def remove_from_wishlist(request,pk):
+	product=Product.objects.get(pk=pk)
+	user=User.objects.get(email=request.session['email'])
+	wishlist=Wishlist.objects.get(user=user,product=product)
+	wishlist.delete()
+	return redirect('wishlist')
+
+
+def add_to_cart(request,pk):
+	product=Product.objects.get(pk=pk)
+	user=User.objects.get(email=request.session['email'])
+	Cart.objects.create(
+		user=user,
+		product=product,
+		product_price=product.product_price,
+		product_qty=1,
+		total_price=product.product_price
+	)
+	return redirect('cart')
+
+def cart(request):
+	net_price=0
+	user=User.objects.get(email=request.session['email'])
+	carts=Cart.objects.filter(user=user,payment_status=False)
+	request.session['cart_count']=len(carts)
+	for i in carts:
+		net_price=net_price+i.total_price
+	return render(request,'cart.html',{'carts':carts,'net_price':net_price})
+	
+def remove_from_cart(request,pk):
+	product=Product.objects.get(pk=pk)
+	user=User.objects.get(email=request.session['email'])
+	cart=Cart.objects.get(user=user,product=product,payment_status=False)
+	cart.delete()
+	return redirect('cart')
+
+def change_qty(request):
+	pk=int(request.POST['pk'])
+	cart=Cart.objects.get(pk=pk)
+	product_qty=int(request.POST['product_qty'])
+	cart.product_qty=product_qty
+	cart.total_price=cart.product_price*product_qty
+	cart.save()
+	return redirect('cart')
+
+
+def myorder(request):
+	user=User.objects.get(email=request.session['email'])
+	carts=Cart.objects.filter(user=user,payment_status=True)
+	return render(request,'myorder.html',{'carts':carts})
+
+
+def seller_view_order(request):
+	myorder=[]
+	seller=User.objects.get(email=request.session['email'])
+	carts=Cart.objects.filter(payment_status=True)
+	for i in carts:
+		if i.product.seller == seller:
+			myorder.append(i)
+	return render(request,'seller-view-order.html',{'myorder':myorder})
